30831a00ef985a6e2a8a1a3badbfbaaa4dd26c5a
Ben Banks <ben.banks@me.com>
2019-06-26 20:49:58 +0100

    Add logger

--- src/main/java/com/banksb95/fixfriend/FixFriendMain.java
+++ src/main/java/com/banksb95/fixfriend/FixFriendMain.java
@@ -1,9 +1,14 @@
 package com.banksb95.fixfriend;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 class FixFriendMain{
 
+    private final static Logger logger = LogManager.getLogger(new FixFriendMain().getClass());
+
     public static void main(String args[]){
-        System.out.println("Hello world!");
+        logger.info("Hello world!");
     }
 
 }
